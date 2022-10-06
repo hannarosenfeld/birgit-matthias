fd4518b5653b3f825b19007771e3c4bdd3295c19
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2022-10-06 12:12:29 -0400

    Navbar menu float right.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -14,7 +14,7 @@ function BasicExample() {
           <section>MALEREI</section>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
+        <Navbar.Collapse style={{flexGrow: "0", alignItems: "end"}} id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#link">Link</Nav.Link>
