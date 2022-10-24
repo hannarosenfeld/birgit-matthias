594d817f17034eaaf23109d0ddf8aa6f9d007960
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2022-10-24 12:11:40 -0400

    Style Galerie.

--- src/components/Galerie.js
+++ src/components/Galerie.js
@@ -1,6 +1,17 @@
 import React from "react"
 import { StaticQuery, graphql } from "gatsby"
 
+const galerieStyle = {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-between"
+}
+const bildbox = {
+    border: "2px solid",
+    width: "19vw",
+    height: "19vw",
+    marginBottom: "2em"
+}
 
 export default function Galerie() {
     return(
@@ -23,8 +34,13 @@ export default function Galerie() {
                   <div style={{border: "2px solid blue", width: "100%", heigth: "45vw"}}>
                     {rubriken.nodes.map(rubrik => {
                         return(
-                            <div>{rubrik.bilder.map(bild => (
-                                <div>{bild.title}</div>
+                            <div style={galerieStyle} id="galerie-list">
+                              {rubrik.bilder.map(bild => (
+                                  <div style={bildbox} className="galerie-list-box">
+                                  <div style={{width: "100%"}}>
+                                      <div className="centered" style={{fontSize: "85%"}}>{bild.title}</div>
+                                    </div>
+                                  </div>
                         )
                                          )}</div>
                         )
