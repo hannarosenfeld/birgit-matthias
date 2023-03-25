809411a06a82709fdf7c3b05d530af1f8ad9cc51
darayaq-01 <daniel@darayaq.de>
2023-03-25 20:21:50 +0100

    footer modules

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,39 +4,30 @@ import { Link } from "gatsby"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {  faFacebookF ,faInstagram } from '@fortawesome/free-brands-svg-icons'
 
+import { footer, footerNav } from "./footer.module.css"
 import "../../style/global.css"
-
-
-const footer = {
-    bottom: "0",
-    height: "2.5rem",
-    display: "flex",
-    justifyContent: "space-between",
-    width: "100%",
-    maxWidth: "75vw",
-    alignItems: "center",
-    marginTop: "2em",
-}
-
-const footerNav = {
-    display: "flex",
-    justifyContent: "space-between",
-    float: "left",
-}
-
 
 export default function Footer() {
     return (
-        <div className="footer" style={footer}>
-          <ul style={footerNav}>
-            <li style={{marginLeft: "-2em"}}>
-              <Link to="/impressum">IMPRESSUM</Link>
+        <div className={footer}>
+          <ul className={footerNav}>
+            <li>
+              <Link 
+              to="/impressum">IMPRESSUM</Link>
             </li>
             <li>
-              <Link to="/datenschutz">DATENSCHUTZ</Link>
+              <Link 
+              to="/datenschutz">DATENSCHUTZ</Link>
             </li>
-            <li><a href="#!"><FontAwesomeIcon icon={faInstagram} /></a></li>
-            <li><a href="https://www.facebook.com/BirgitMatthiasKunst/"><FontAwesomeIcon icon={faFacebookF} /></a></li>
+            <li>
+              <a href="#!"><FontAwesomeIcon icon={faInstagram} />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.facebook.com/BirgitMatthiasKunst/">
+                <FontAwesomeIcon icon={faFacebookF} />
+              </a>
+            </li>
           </ul>
         </div>
     )
