5ea5e798d8409d8019893eafa9e39c2792c49346
darayaq-01 <daniel@darayaq.de>
2023-03-28 09:13:13 +0200

    basic style

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,49 +1,40 @@
 import React from 'react'
 
+import { background, textStyle } from './banner.module.css'
+import '../../style/global.css'
+
+const content = (
+  <>
+    <p>
+      Ich arbeite hauptsächlich abstrakt mit Acryl auf Leinwand. Die Formate
+      sind in der Regel groß gewählt.
+    </p>
+    <p>
+      Abstrakte Malerei ist für mich Komponieren und Erfinden, Verwenden und
+      Verfremden bekannter Seh- und Malweisen, Spielen mit Impulsen, die bei dem
+      Malprozess entstehen und Schaffen neuer Realitäten – vielleicht
+      vergleichbar mit der Komposition von Musik, nur mit Farbe und Form.
+    </p>
+    <p>
+      Den Begriff der „halbabstrakten Malerei“ habe ich kreiert, um den Stil
+      meiner Bilder zu definieren.
+    </p>
+    <p>
+      Vordergründig abstrakte Bilder erhalten eine zusätzliche Ebene, indem sie
+      konkrete Inhalte erahnen lassen.
+    </p>
+    <p>
+      Mir geht es um die tiefe Schönheit der Welt, der Menschen und der Natur,
+      diese zu ergründen und mit Farben sichtbar zu machen.
+    </p>
+  </>
+)
+
 export default function Banner() {
   return (
-    <div
-      style={{
-        background: 'aquamarine',
-        minHeight: '95vh'
-      }}
-    >
-      <div
-        style={{
-          width: '65vw',
-          margin: '0 auto',
-          padding: '2em 0'
-        }}
-      >
-        <div
-          style={{
-            width: '50%'
-          }}
-        >
-          <br />
-          Ich arbeite hauptsächlich abstrakt mit Acryl auf Leinwand. Die Formate
-          sind in der Regel groß gewählt.
-          <br />
-          <br />
-          Abstrakte Malerei ist für mich Komponieren und Erfinden, Verwenden und
-          Verfremden bekannter Seh- und Malweisen, Spielen mit Impulsen, die bei
-          dem Malprozess entstehen und Schaffen neuer Realitäten – vielleicht
-          vergleichbar mit der Komposition von Musik, nur mit Farbe und Form.
-          <br />
-          <br />
-          Den Begriff der „halbabstrakten Malerei“ habe ich kreiert, um den Stil
-          meiner Bilder zu definieren.
-          <br />
-          <br />
-          Vordergründig abstrakte Bilder erhalten eine zusätzliche Ebene, indem
-          sie konkrete Inhalte erahnen lassen.
-          <br />
-          <br />
-          Mir geht es um die tiefe Schönheit der Welt, der Menschen und der
-          Natur, diese zu ergründen und mit Farben sichtbar zu machen.
-          <br />
-          <br />
-        </div>
+    <div className={background}>
+      <div className="column-inner">
+        <div className={textStyle}>{content}</div>
       </div>
     </div>
   )
